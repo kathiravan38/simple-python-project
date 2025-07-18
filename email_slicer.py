d97b72ae1ff626f23c2356055537a93ab1e3cf64
kathiravan38 <kathiravans08032006@gmail.com>
2025-07-18 13:37:14 +0530

    create email slicer

--- email_slicer.py
+++ email_slicer.py
@@ -1,5 +1,5 @@
 email=input("enter your email address:")
 index=email.index("@")
 username=email[:index]
-domain=email[index:]
+domain=email[index+1:]
 print(f"username:{username}\ndomain:{domain}")
